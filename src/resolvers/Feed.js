7218d50573b62c5fd1618c0acecd34318b927afb
kpapachristou <kiriakos.papachristou@trivago.com>
2018-02-26 14:30:09 +0100

    Added a new Feed Type and added it on the schema

---
+++ src/resolvers/Feed.js
@@ -0,0 +1,9 @@
+function links(parent, args, context, info) {
+    const { linkIds } = parent;
+
+    return context.db.query.links({ where: { id_in: linkIds } }, info);
+}
+
+module.exports = {
+    links,
+};
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,10 +1,18 @@
-function feed(parent, args, context, info) {
+async function feed(parent, args, context, info) {
     const {filter, first, skip} = args;
     const where = filter
         ? {OR: [{url_contains: filter}, {description_contains: filter}]}
         : {};
 
-    return context.db.query.links({first, skip, where}, info)
+    const allLinks = await context.db.query.links({});
+    const count = allLinks.length;
+
+    const queriedLinks = await context.db.query.links({ first,skip, where });
+
+    return {
+        linkIds: queriedLinks.map(link => link.id),
+        count
+    }
 }
 
 module.exports = {
